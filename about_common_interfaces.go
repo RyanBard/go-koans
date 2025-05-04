4315d37cf10ff5d4a207d1dbdf278a87f0d005a4
Ryan Bard <john.ryan.bard@gmail.com>
2025-05-04 19:47:38 -0400

    Add more notes and info for common interfaces

--- about_common_interfaces.go
+++ about_common_interfaces.go
@@ -1,6 +1,9 @@
 package go_koans
 
-import "bytes"
+import (
+	"bytes"
+	"io"
+)
 
 func aboutCommonInterfaces() {
 	{
@@ -16,6 +19,9 @@ func aboutCommonInterfaces() {
 		   $ godoc -http=:8080
 		   $ open http://localhost:8080/pkg/io/
 		   $ open http://localhost:8080/pkg/bytes/
+		   $ # or look at:
+		   $ open https://pkg.go.dev/io
+		   $ open https://pkg.go.dev/bytes
 		*/
 
 		assert(out.String() == "hello world") // get data from the io.Reader to the io.Writer
@@ -28,5 +34,7 @@ func aboutCommonInterfaces() {
 		out := new(bytes.Buffer)
 
 		assert(out.String() == "hello") // duplicate only a portion of the io.Reader
+		b, _ := io.ReadAll(in)
+		assert(string(b) == " world")
 	}
 }
